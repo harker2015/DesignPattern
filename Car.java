f88cd6d2ecff761a7a19e876bc138e38738cb2be
hacker <306539332@qq.com>
2022-11-27 13:59:29 +0800

    remove root file

--- Car.java
+++
@@ -1,7 +0,0 @@
-public interface Car {
-	public String getCarName();
-	public String getCarFeatures();
-	public double getCarEngineSize();
-	public double getCarPrice();
-	public double getCarCO2Emissions();
-} // End of class
--- SUV.java
+++
@@ -1,7 +0,0 @@
-public interface SUV {
-	public String getSUVName();
-	public String getSUVFeatures();
-	public double getSUVEngineSize();
-	public double getSUVPrice();
-	public double getSUVCO2Emissions();
-} // End of class
